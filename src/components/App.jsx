dd0730697dba4db2666107edc5d261deea8ade63
gladja <gladkih.alexander@gmail.com>
2024-03-16 10:23:18 +0200

    add tailwind

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,12 @@
-// import { useState } from "react";
-// import reactLogo from "../assets/react.svg";
-// import viteLogo from "/vite.svg";
-import "./App.css";
+// import "./App.css";
+import { Container } from "./Container/Container";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Star</h1>
+      <Container>
+        <h1 className="text-6xl font-bold text-green-600">Star!</h1>
+      </Container>
     </>
   );
 }
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,4 @@
+// eslint-disable-next-line react/prop-types
+export const Container = ({ children }) => {
+  return <section className="flex">{children}</section>;
+};
